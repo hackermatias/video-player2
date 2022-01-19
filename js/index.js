a563a06a18ef16f43fbd29f84fff99f46c397dd7
matias Morales <58788117+hackermatias@users.noreply.github.com>
2022-01-19 16:26:35 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,61 @@
+  const $video = document.querySelector('#video')
+  const $play = document.querySelector('#play')
+  const $pause = document.querySelector('#pause')
+  const $backward = document.querySelector('#backward')
+  const $forward = document.querySelector('#forward')
+
+  $play.addEventListener('click', handlePlay)
+  $pause.addEventListener('click', handlePause)
+
+  function handlePlay() {
+    $video.play()
+    $play.hidden = true
+    $pause.hidden = false
+    console.log('le diste click al boton play');
+  }
+
+  function handlePause() {
+    $video.pause()
+    $pause.hidden = true
+    $play.hidden = false
+    console.log('le diste click al boton pause');
+  }
+
+
+  $backward.addEventListener('click',handleBackward)
+
+  function handleBackward() {
+    $video.currentTime = $video.currentTime - 10 
+    $video.currentTime -= 10
+    console.log('para atras 10  segundos', $video.currentTime);
+
+  }
+
+  $forward.addEventListener('click',handleForward)
+
+  function handleForward() {
+    $video.currentTime = $video.currentTime + 10 
+    console.log('para adelante 10  segundos',$video.currentTime);
+
+  }
+
+  const $progress = document.querySelector('#progress')
+  $video.addEventListener('loadedmetadata', handleLoaded)
+  $video.addEventListener('timeupdate', handleTimeUpdate)
+
+  function handleLoaded() {
+    $progress.max = $video.duration
+    console.log('a cargado mi video ', $video.duration);
+  }
+
+
+  function handleTimeUpdate () {
+    $progress.value = $video.currentTime
+   //console.log('tiempo actualizado', $video.currentTime);
+  }
+
+  $progress.addEventListener('input', handleInput)
+
+  function handleInput() {
+    console.log($progress.value);
+  }
